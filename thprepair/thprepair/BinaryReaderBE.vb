39955b790b25997353a9f0137518a443d42756f7
shawarmakarma <shawarmakarma@7dd2f8ad-8857-619b-c375-268c912672e4>
2010-05-16 14:25:38 +0000

    First version that can load and maybe repair.  Still working on playing the video

---
+++ thprepair/thprepair/BinaryReaderBE.vb
@@ -0,0 +1,21 @@
+﻿Module BinaryReaderBE
+    <System.Runtime.CompilerServices.Extension()> _
+    Public Function ReadUInt32BE(ByVal b As System.IO.BinaryReader) As UInt32
+        Dim temp_bytes() As Byte = b.ReadBytes(4)
+
+        If BitConverter.IsLittleEndian Then
+            Array.Reverse(temp_bytes)
+        End If
+        Return BitConverter.ToUInt32(temp_bytes, 0)
+    End Function
+
+    <System.Runtime.CompilerServices.Extension()> _
+    Public Function ReadSingleBE(ByVal b As System.IO.BinaryReader) As Single
+        Dim temp_bytes() As Byte = b.ReadBytes(4)
+
+        If BitConverter.IsLittleEndian Then
+            Array.Reverse(temp_bytes)
+        End If
+        Return BitConverter.ToSingle(temp_bytes, 0)
+    End Function
+End Module
