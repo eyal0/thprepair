bdc16e9e84f72cd32a64fd62f2a1ace0ff72fcf9
shawarmakarma <shawarmakarma@7dd2f8ad-8857-619b-c375-268c912672e4>
2010-05-18 10:31:39 +0000

    It's finally working.

--- thprepair/thprepair/BinaryReaderBE.vb
+++ thprepair/thprepair/BinaryReaderBE.vb
@@ -18,4 +18,24 @@
         End If
         Return BitConverter.ToSingle(temp_bytes, 0)
     End Function
+
+    <System.Runtime.CompilerServices.Extension()> _
+    Public Sub WriteBE(ByVal bw As System.IO.BinaryWriter, ByVal data As UInt32)
+        Dim temp_bytes() As Byte = BitConverter.GetBytes(data)
+
+        If BitConverter.IsLittleEndian Then
+            Array.Reverse(temp_bytes)
+        End If
+        bw.Write(temp_bytes)
+    End Sub
+
+    <System.Runtime.CompilerServices.Extension()> _
+    Public Sub WriteBE(ByVal bw As System.IO.BinaryWriter, ByVal data As Single)
+        Dim temp_bytes() As Byte = BitConverter.GetBytes(data)
+
+        If BitConverter.IsLittleEndian Then
+            Array.Reverse(temp_bytes)
+        End If
+        bw.Write(temp_bytes)
+    End Sub
 End Module
